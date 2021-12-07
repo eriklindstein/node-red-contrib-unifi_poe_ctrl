1ec51946e85a2bea894caf5db8ff6e33af463ee4
Erik Lindstein <erik@embsec.com>
2021-12-07 12:51:32 +0100

    local changes

--- unifi_poe_ctrl.js
+++ unifi_poe_ctrl.js
@@ -13,8 +13,17 @@ module.exports = function (RED) {
             else if(msg.payload.cmd == "auto"){
                 cmd = ["telnet localhost 23", "enable", "configure", "interface 0/" + msg.payload.interface, "poe opmode auto", "exit", "exit", "exit", "exit", "exit"];
             }
-            else {
-                cmd = ["telnet localhost 23", "enable", "show poe status 0/" + msg.payload.interface, "exit", "exit", "exit", "exit"];
+            else if(msg.payload.cmd == "mac-addr-table"){
+                cmd = ["telnet localhost 23", "enable", "show mac-addr-table", "exit", "exit", "exit", "exit"];
+            }
+            else if(msg.payload.cmd == "poe_status"){
+                cmd = ["telnet localhost 23", "enable", "show poe status 0/" + msg.payload.interface || 1, "exit", "exit", "exit", "exit"];
+            }
+            else if(msg.payload.cmd == "switch_temp"){
+                cmd = ["swctrl env show"];
+            }
+            else if(msg.payload.cmd == "all"){
+                cmd = ["swctrl env show", "telnet localhost 23", "enable", "show poe status all", "exit", "exit", "exit"];
             }
             //host configuration with connection settings and commands
             var host = {
@@ -45,6 +54,22 @@ module.exports = function (RED) {
                         msg.payload.status.consumed_W = status[3];
                         msg.payload.status.volage = status[4];
                         msg.payload.status.current_mA = status[5];
+                    }
+                    else if(msg.payload.cmd == "mac-addr-table"){
+                        const startstr = "--------------";
+                        const stopstr = "(UBNT) #";
+                        var startpos = sessionText.lastIndexOf(startstr) + startstr.length;
+                        var stoppos = sessionText.lastIndexOf(stopstr);
+                        var status = sessionText.substring(startpos,stoppos).replace(/\r\n/g, "").replace(/\n/g, "").trim().split(/\s+/);
+                        msg.payload.raw_data = status;
+                    }
+                    else if(msg.payload.cmd == "switch_temp"){
+                        const startstr = "General Temperature (C):";
+                        const stopstr = "Temp Sensor";
+                        var startpos = sessionText.lastIndexOf(startstr) + startstr.length;
+                        var stoppos = sessionText.lastIndexOf(stopstr);
+                        var status = sessionText.substring(startpos,stoppos).replace(/\r\n/g, "").replace(/\n/g, "").trim().split(/\s+/);
+                        msg.payload.switch_temp = parseFloat(status[0]);
                     }
                     msg.payload.response = sessionText;
                     node.send(msg);
